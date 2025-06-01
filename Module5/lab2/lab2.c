f08a4fe242b339290fbd6b1e8db44f721d52c291
vladimir-yudin <vladimir_yudin04@vk.com>
2025-06-02 05:47:50 +0700

    lab2

---
+++ Module5/lab2/lab2.c
@@ -0,0 +1,57 @@
+#include <linux/module.h>
+#include <linux/kernel.h>
+#include <linux/proc_fs.h>
+#include <linux/sched.h>
+#include <linux/uaccess.h>
+#include <linux/slab.h>
+ 
+static int len, temp;
+static const int SIZE = 10;
+static char *msg;
+ 
+static ssize_t read_proc(struct file *filp, char *buf, size_t count, loff_t *offp ) {
+    if(count > temp) {
+        count = temp;
+    }
+    temp = temp - count;
+    if (copy_to_user(buf, msg, count)){
+        return -1;
+    }
+    if(count == 0)
+        temp = len;
+    return count;
+}
+ 
+static ssize_t write_proc(struct file *filp, const char *buf, size_t count, loff_t *offp) {
+    if (copy_from_user(msg, buf, count)){
+        return -1;
+    }
+    len = count;
+    temp = len;
+    return count;
+}
+ 
+static const struct proc_ops proc_fops = {
+    proc_read: read_proc,
+    proc_write: write_proc,
+};
+ 
+static void create_new_proc_entry(void) { //use of void for no arguments is compulsory now
+    proc_create("lab2", 0, NULL, &proc_fops);
+    msg = kmalloc(SIZE * sizeof(char), GFP_KERNEL);
+}
+
+static int proc_init (void) {
+    create_new_proc_entry();
+    return 0;
+}
+ 
+static void proc_cleanup(void) {
+    remove_proc_entry("lab2", NULL);
+    kfree(msg);
+}
+ 
+MODULE_LICENSE("GPL");
+MODULE_AUTHOR("Yudin V.");
+module_init(proc_init);
+module_exit(proc_cleanup);
